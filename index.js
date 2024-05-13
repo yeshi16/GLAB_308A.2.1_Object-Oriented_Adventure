31b595617d9378643b277357c81c4c5044a02148
m-s1621 <mebashimelis@gmail.com>
2024-05-13 12:49:19 -0400

    part two

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const adventurer = {
     companion:{
         name: "Leo",
         type: "Cat",
-        friend: {
+        companion: {
             name: "Frank",
             type: "Flea",
             belongings: ["hat", "sunglasses"] 
@@ -22,4 +22,26 @@ const adventurer = {
 
 adventurer.inventory.forEach(item => {console.log(item)})
 
-adventurer.roll()
+adventurer.roll();
+
+// Part 2: Class Fantasy
+
+class Character {
+    constructor(name){
+        this.name = name;
+        this.health = 100;
+        this.inventory = [];
+    }
+    roll (mod = 0){
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`{this.name} rolled a ${result}.`)
+    }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo")
+robin.companion.type = "Cat"
+robin.companion.companion = new Character("Frank")
+robin.companion.companion.type = "Flea"
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
