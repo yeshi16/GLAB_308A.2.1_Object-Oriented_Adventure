460172905f82e7886d4e6c597b1c27444f71d17a
m-s1621 <mebashimelis@gmail.com>
2024-05-13 12:42:29 -0400

    part one

---
+++ index.js
@@ -0,0 +1,25 @@
+// Part 1: Humble Beginnings
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion:{
+        name: "Leo",
+        type: "Cat",
+        friend: {
+            name: "Frank",
+            type: "Flea",
+            belongings: ["hat", "sunglasses"] 
+        }
+    },
+
+    roll (mod = 0){
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`{this.name} rolled a ${result}.`)
+    }
+}
+
+adventurer.inventory.forEach(item => {console.log(item)})
+
+adventurer.roll()
